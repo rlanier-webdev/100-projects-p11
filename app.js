bcd7b84c472c65a442b4bf2a3a9036e1fddb0e46
RaShunda Lanier <61757693+rlanier-webdev@users.noreply.github.com>
2021-03-15 20:51:32 -0400

    final updates

--- app.js
+++ app.js
@@ -7,19 +7,11 @@ btn.addEventListener('click', function(){
 });
 
 function convert() {
-    
-/*     
-    Ex: Convert height of 5 feet 2 inches to centimeters.
-
-    First, convert 5 feet to inches: 5 feet × 12 inches/foot = 60 inches
-    Add up our inches: 60 + 2 = 62 inches
-    Convert inches to cm: 62 inches × 2.54 cm/inch = 157.48 cm 
-*/
     var feet = Number(document.getElementById("feet").value);
     var inches = Number(document.getElementById("inches").value);
 
     var cm = 2.54 * ((feet * 12) + inches);
-    
+
     document.getElementById("output__area").innerHTML = cm + " cm";
 
 }
